c927033e12c632fe2a7c864f7db49a5108819e7c
Joseph-Cecil <josephgodwins015@gmail.com>
2024-02-20 09:48:59 +0000

    Added Authentication to frontend

---
+++ frontend/src/components/Username.tsx
@@ -0,0 +1,32 @@
+import { DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu"
+import { DropdownMenu } from "./ui/dropdown-menu"
+import { CircleUserRound } from "lucide-react"
+import { useAuth0 } from "@auth0/auth0-react"
+import { Link } from "react-router-dom"
+import { Separator } from "./ui/separator"
+import { Button } from "./ui/button"
+
+const UsernameMenu = () => {
+    const {user, logout} = useAuth0();
+  return (
+    <DropdownMenu>
+        <DropdownMenuTrigger className="flex items-center px-3 font-bold hover:text-orange-500 gap-2">
+            <CircleUserRound className="text-orange-500"/>
+            {"Hello " + user?.name}
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+            <DropdownMenuItem>
+            <Link to="/user-profile" className="font-bold hover:text-orange-500">
+                User Profile
+            </Link>
+            </DropdownMenuItem>
+            <Separator/>
+            <DropdownMenuItem>
+            <Button className="flex flex-1 font-bold bg-white text-black-500 hover:bg-orange-450" onClick={() => logout()}>Sign Out</Button>
+            </DropdownMenuItem>
+        </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
+
+export default UsernameMenu
