f748ff114ba9524139a81c0846362d6b64dfe3e5
Joseph-Cecil <josephgodwins015@gmail.com>
2024-02-21 22:49:34 +0000

    second badge of connectin frontend to backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express, {Request, Response} from 'express';
 import cors from "cors";
 import "dotenv/config";
 import mongoose from 'mongoose';
+import myUserRoute from "./routes/myUserRoutes";
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => 
     console.log("connected to a database"));
@@ -10,9 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/test", async(req: Request, res:Response ) => {
-    res.json({message: "Hello"});
-});
+app.use("/api/my/user", myUserRoute)
 
 app.listen(4000, () => {
     console.log(`Sever started on localhost 4000`)
---
+++ backend/src/routes/myUserRoutes.ts
@@ -0,0 +1,9 @@
+import express from "express";
+import myUserController from "../controllers/myUserController";
+
+const router = express.Router();
+
+router.post("/", myUserController.createCurrentUser);
+
+
+export default router;
