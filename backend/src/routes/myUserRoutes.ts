e23cada71424527b7b35b9abc4290aaca1e45602
Joseph-Cecil <josephgodwins015@gmail.com>
2024-02-28 23:06:18 +0000

    Added Authentication to backend and frontend

--- backend/src/routes/myUserRoutes.ts
+++ backend/src/routes/myUserRoutes.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import myUserController from "../controllers/myUserController";
+import { jwtCheck } from "../middleware/auth";
 
 const router = express.Router();
 
-router.post("/", myUserController.createCurrentUser);
+router.post("/", jwtCheck, myUserController.createCurrentUser);
 
 
 export default router;
--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,5 +1,5 @@
-import { useCreateMyUser } from "@/api/MyUserApi";
-import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
+import {Auth0Provider } from "@auth0/auth0-react";
+import { useNavigate } from "react-router-dom";
 
 type Props = {
   children: React.ReactNode;
@@ -7,28 +7,26 @@ type Props = {
 
 const Auth0ProviderWithNavigate = ({ children }: Props) => {
 
-  const { createUser} = useCreateMyUser(); 
 
+  const navigate = useNavigate();
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
   const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
+  const audience = import.meta.env.VITE_AUTH0_AUDIENCE
 
-  if (!domain || !clientId || !redirectUri) {
+  if (!domain || !clientId || !redirectUri || !audience) {
     throw new Error("Unable to initialise auth");
   }
 
-  const onRedirectCallback = (appState?: AppState, user?: User) => {
-    console.log("User: ", user)
-    if(user?.sub && user?.email) {
-      createUser({auth0Id: user.sub, email: user.email})
-    }
+  const onRedirectCallback = () => {
+    navigate("/auth-callback")
   }
 
   return (
     <Auth0Provider
       domain={domain}
       clientId={clientId}
-      authorizationParams={{ redirect_uri: redirectUri }}
+      authorizationParams={{ redirect_uri: redirectUri, audience }}
       onRedirectCallback={onRedirectCallback}
     >
         {children}
