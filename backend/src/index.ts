b66b06351c132d0222647b8d1003627679f077ea
Joseph-Cecil <josephgodwins015@gmail.com>
2024-03-07 04:21:49 +0000

    Package.json commit

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,6 +11,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get("/health", async (req: Request, res: Response) => {
+    res.send({message: "Health is OK"})
+})
+
 app.use("/api/my/user", myUserRoute)
 
 app.listen(4000, () => {
